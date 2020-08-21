c42379497b43dde8ea53232803067e6d6825b059
Braxton125 <braxtonr126@gmail.com>
2020-08-21 05:40:55 +0000

    Created Micky Mouse

---
+++ HelloWorld.java
@@ -0,0 +1,46 @@
+//Braxton Renz
+//Ap computer Science Ap
+//8/20/20
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("______________$$$$$$$");
+    System.out.println("_____________$$$$$$$$$");
+    System.out.println("____________$$$$$$$$$$$");
+    System.out.println("____________$$$$$$$$$$$");
+    System.out.println("____________$$$$$$$$$$$");
+    System.out.println("_____________$$$$$$$$$");
+    System.out.println("_____$$$$$$_____$$$$$$$$$$");
+    System.out.println("____$$$$$$$$__$$$$$$_____$$$");
+    System.out.println("___$$$$$$$$$$$$$$$$_________$");
+    System.out.println("___$$$$$$$$$$$$$$$$_________$");
+    System.out.println("___$$$$$$$$$$$$$$$$_____$$$_$");
+    System.out.println("___$$$$$$$$$$$__________$$$_$_____$$");
+    System.out.println("____$$$$$$$$$____________$$_$$$$_$$$$");
+    System.out.println("______$$$__$$__$$$______________$$$$");
+    System.out.println("___________$$____$_______________$");
+    System.out.println("____________$$____$______________$");
+    System.out.println("_____________$$___$$$__________$$");
+    System.out.println("_______________$$$_$$$$$$_$$$$$");
+    System.out.println("_______________$$__$$$$$$$$$$$___$_$");
+    System.out.println("______________$$$__$___$$$______$$$$");
+    System.out.println("______________$$$_$__________$$_$$$$");
+    System.out.println("______________$$$$$_________$$$$_$_$");
+    System.out.println("_______________$$$$__________$$$__$$");
+    System.out.println("_____$$$$_________$________________$");
+    System.out.println("___$$$___$$______$$$_____________$$");
+    System.out.println("__$___$$__$$_____$__$$$_____$$__$$");
+    System.out.println("_$$____$___$_______$$$$$$$$$$$$$");
+    System.out.println("_$$_____$___$_____$$$$$_$$___$$$");
+    System.out.println("_$$_____$___$___$$$$____$____$$");
+    System.out.println("__$_____$$__$$$$$$$____$$_$$$$$");
+    System.out.println("__$$_____$___$_$$_____$__$__$$$$$$$$$$$$");
+    System.out.println("___$_____$$__$_$_____$_$$$__$$__$______$$$");
+    System.out.println("____$$_________$___$$_$___$$__$$_________$");
+    System.out.println("_____$$_$$$$___$__$$__$__________________$");
+    System.out.println("______$$____$__$$$____$__________________$");
+    System.out.println("_______$____$__$_______$$______________$$");
+    System.out.println("_______$$$$_$$$_________$$$$$$$__$$$$$$");
+    System.out.print("__________$$$_________________$$$$$");
+/*My art work is of Micky Mouse*/
+  }
+}
